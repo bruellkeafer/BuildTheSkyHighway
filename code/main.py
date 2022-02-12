34ddf877a69625e1d5cc8887347a5193e1e95270
Tim Martins <s80691@beuth-hochschule.de>
2022-02-12 23:29:38 +0100

    init

---
+++ code/main.py
@@ -0,0 +1,122 @@
+import random as rd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Building:
+
+    ReachableAntennas = []
+    Score = 0
+
+    def __init__(self,x,y,latencyWeight,connectionSpeedWeight):
+        self.pos = [x,y]
+        self.latencyWeight = latencyWeight
+        self.connectionSpeedWeight = connectionSpeedWeight
+    
+    def findBestAntenna(self,antennas):
+
+         #if(distance(self.X,self.Y,a.X,a.Y) <= a.Range):
+                
+
+        bestAntenna = [0, None]
+        for antenna in antennas:                
+            score = getScore(self, antenna)
+            if(distance(self, antenna) > antenna.range):
+                score *= 0.000001
+            if(score > bestAntenna[0]):
+                bestAntenna = [score,antenna]
+        return bestAntenna
+
+class Antenna:    
+    def __init__(self, rangeValue, connectionSpeed):
+      self.range = rangeValue
+      self.connectionSpeedWeight = connectionSpeed
+      self.pos = [None, None]
+      self.assignedBuildings = []
+    
+    def assignBuilding(self,building):
+      self.assignedBuildings.append(building)
+
+    def listReachableBuildings(self,buildingsList):
+        for a in buildingsList:
+            if(distance(self.X,self.Y,a.X,a.Y) <= a.Range):
+                self.ReachableAntennas.append(a)
+
+class Clustering:
+    def __init__(self, antennas, buildings):
+      self.antennas = antennas
+      self.buildings = buildings
+      self.NumberOfAntennas = len(antennas)
+      for antenna in self.antennas:
+        antenna.pos = [rd.randint(0,MapXLength), rd.randint(0,MapYLength)]
+      
+    def clusterStep(self):
+      #build cluster groups
+      for building in self.buildings:
+
+        _ , antenna = building.findBestAntenna(self.antennas)
+        antenna.assignBuilding(building)
+
+      for antenna in self.antennas:
+        antenna.pos = np.round(np.average(list(map(lambda building : building.pos[0], antenna.assignedBuildings)))),np.round(np.average(list(map(lambda building : building.pos[1], antenna.assignedBuildings))))
+        #antenna.pos = np.round(np.average(list(map(lambda building : building.pos, antenna.assignedBuildings)),axis = 1))
+
+def distance(building,antenna):
+    return np.linalg.norm(np.array(building.pos) - np.array(antenna.pos))
+
+def totalScore(buildings, reward):
+    if(any(hasattr(x, 'Score') for x in buildings)):
+        return 0
+    return sum(list(map(lambda x : x.Score, buildings))) + reward
+
+def getScore(building,antenna):
+
+  return (building.connectionSpeedWeight * antenna.connectionSpeedWeight) - (building.latencyWeight * distance(antenna,building))
+
+def connectBuildingToAntenna(building):
+    building.connectedAntenna = max(list(map(lambda x : getScore(building,x), building.ReachableAntennas)))
+
+file=open("C:\\Users\\Tim Martins\\Desktop\\Projekte\\Programmieren\\ReplyChallenges\\TapTapTap\\BuildTheSkyHighway\\data_scenarios_a_example.in","r")
+
+string = file.read()
+inputs = string.split("\n")
+#Line 1 - Map Read
+MapScale = inputs[0].split(" ")
+MapXLength = int(MapScale[0])
+MapYLength = int(MapScale[1])
+#Line 2 - Resources Read
+ResourceInformation = inputs[1].split(" ")
+BuildingsCount = int(ResourceInformation[0])
+AntennasCount = int(ResourceInformation[1])
+RewardValue = int(ResourceInformation[2])
+#Buildings Read
+Buildings = []
+for i in range(BuildingsCount):
+    buildingInformation = inputs[i+2].split(" ")
+    Buildings.append(Building(
+        int(buildingInformation[0]),
+        int(buildingInformation[1]),
+        int(buildingInformation[2]),
+        int(buildingInformation[3]),
+    ))
+#Antennas Read
+Antennas = []
+for i in range(AntennasCount):
+    antennaInformation = inputs[i+BuildingsCount+2].split(" ")
+    Antennas.append(Antenna(
+        int(antennaInformation[0]),
+        int(antennaInformation[1])
+    ))
+
+def plot_map(antennas,buildings):
+    plt.scatter(np.array(list(map(lambda building : building.pos, buildings)))[:,0], np.array(list(map(lambda building : building.pos, buildings)))[:,1])
+    plt.scatter(np.array(list(map(lambda antenna : antenna.pos, antennas)))[:,0], np.array(list(map(lambda antenna : antenna.pos, antennas)))[:,1])
+    plt.show()
+
+cl = Clustering(Antennas,Buildings)
+
+#plot_map(Antennas,Buildings)
+
+cl.clusterStep()
+for a in cl.antennas:
+    print(a.pos)
+plot_map(Antennas,Buildings)
